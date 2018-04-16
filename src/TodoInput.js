fdee82622a114995ee340812a416938c55df379e
[IMJ] Yukiko Hori <porico@imjp.co.jp>
2018-04-16 18:30:54 +0900

    addTodoメソッドを呼ぶhandleClickメソッドを追加

--- src/TodoInput.js
+++ src/TodoInput.js
@@ -3,12 +3,23 @@ import React, { Component } from 'react';
 // [TODO App]
 // - todoを入力するだけのコンポーネント
 class TodoInput extends Component {
+
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+    // e.prependDefault(); // Eventオブジェクトを引数で受け取れる
+    this.props.addTodo('新規Todo');
+  }
+
   render() {
 
     return(
       <div>
         <input type="text" placeholder="新規TODOを入力してください" />
-        <button>登録</button>
+        <button onClick={this.handleClick}>登録</button>
       </div>
     )
   }
